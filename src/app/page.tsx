72382d6a2a1fa079b6eee2fa8dfb6de44c0fd6f8
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-08-19 09:32:55 +0600

    voice record done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "./_components/banner";
+import VoiceRecorder from "./_components/VoiceRecorder";
 
 const Page = () => {
   return (
     <div className="bg-black">
       <Banner />
+      <VoiceRecorder />
     </div>
   );
 };
